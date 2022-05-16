20b5ad8d5e7c04442d91f9a5902fc53ad1eff1b8
Admin <admin@MacBook-Air-de-Fabian.local>
2022-05-16 09:29:55 -0500

    refactorCalculatorClass

--- src/main/java/com/falpema/service/PayCalculator.java
+++ src/main/java/com/falpema/service/PayCalculator.java
@@ -1,5 +1,14 @@
 package com.falpema.service;
 
 public class PayCalculator {
-
+	
+	//Strategies of calculating of pay hours
+	
+	private final PayCalculationStrategy payHoursMonToFriday0To9 = new PayHoursMondayFridayCalculationFrom0To9();
+	private final PayCalculationStrategy payHoursMonToFriday9To18 =  new PayHoursMondayFridayCalculationFrom9To18();
+	private final PayCalculationStrategy payHoursMonToFriday18To24 =  new PayHoursMondayFridayCalculationFrom18To24();
+	private final PayCalculationStrategy payHoursSatToSunday0To9 = new PayHoursSaturdaySundayCalculationFrom0To9();
+	private final PayCalculationStrategy payHoursSatToSunday9To18 =  new PayHoursSaturdaySundayCalculationFrom9To18();
+	private final PayCalculationStrategy payHoursSatToSunday18To24 =  new PayHoursSaturdaySundayCalculationFrom18To24();
+	
 }
