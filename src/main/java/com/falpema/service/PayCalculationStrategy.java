9d91fa0d0c9c4531ab211a75ae9bbe77e56b5a80
Admin <admin@MacBook-Air-de-Fabian.local>
2022-05-16 09:12:28 -0500

    concrete strategy Calculates

---
+++ src/main/java/com/falpema/service/PayCalculationStrategy.java
@@ -0,0 +1,5 @@
+package com.falpema.service;
+
+ interface PayCalculationStrategy {
+	double calculatePay(double workHours) ;
+}
---
+++ src/main/java/com/falpema/service/PayCalculationStrategyImpl.java
@@ -0,0 +1,60 @@
+package com.falpema.service;
+
+/**
+ * Concrete Strategys
+ * @author falpema
+ *
+ */
+class PayHoursMondayFridayCalculationFrom12To9 implements PayCalculationStrategy {
+
+	public double calculatePay(double workHours) {
+
+		return workHours*25;
+	}
+
+}
+
+class PayHoursMondayFridayCalculationFrom9To18 implements PayCalculationStrategy {
+
+	public double calculatePay(double workHours) {
+
+		return workHours*15;
+	}
+
+}
+
+class PayHoursMondayFridayCalculationFrom18To24 implements PayCalculationStrategy {
+
+	public double calculatePay(double workHours) {
+
+		return workHours*20;
+	}
+
+}
+
+class PayHoursSaturdaySundayCalculationFrom12To9 implements PayCalculationStrategy {
+
+	public double calculatePay(double workHours) {
+
+		return workHours*30;
+	}
+
+}
+
+class PayHoursSaturdaySundayCalculationFrom9To18 implements PayCalculationStrategy {
+
+	public double calculatePay(double workHours) {
+
+		return workHours*20;
+	}
+
+}
+
+class PayHoursSaturdaySundayCalculationFrom18To24 implements PayCalculationStrategy {
+
+	public double calculatePay(double workHours) {
+
+		return workHours*25;
+	}
+
+}
